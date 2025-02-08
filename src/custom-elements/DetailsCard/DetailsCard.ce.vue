ef59fbb0ca6dff0d891acff29293e32eda5b59d0
Sohype-Khaled <sohype.mop@gmail.com>
2025-02-09 01:55:55 +0200

    add details card

--- src/custom-elements/DetailsCard/DetailsCard.ce.vue
+++ src/custom-elements/DetailsCard/DetailsCard.ce.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="max-w-lg mx-auto bg-white shadow-md rounded-lg p-6">
+  <div class="w-full mx-auto bg-white shadow-md rounded-lg p-6">
     <h2 class="text-xl font-semibold text-gray-800">{{ title }}</h2>
     <p class="text-gray-600 text-base">{{ description }}</p>
 
